660bd4c7e2698152d5842eea50ad46f92ac29172
Chris Rose <offline@offby1.net>
2025-01-11 00:37:36 -0800

    Update the constants to match HA 2025.1

--- custom_components/purpleair/const.py
+++ custom_components/purpleair/const.py
@@ -1,6 +1,14 @@
 """Constants for the Purple Air integration."""
-from homeassistant.const import TEMP_FAHRENHEIT, SIGNAL_STRENGTH_DECIBELS_MILLIWATT, PRESSURE_HPA,PERCENTAGE, DEVICE_CLASS_AQI
 
+from homeassistant.components.sensor import SensorDeviceClass
+from homeassistant.const import (
+    PERCENTAGE,
+    SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+    UnitOfPressure,
+    UnitOfTemperature,
+)
+
+# fmt: off
 AQI_BREAKPOINTS = {
     'pm2_5': [
         { 'pm_low': 500.5, 'pm_high': 999.9, 'aqi_low': 501, 'aqi_high': 999 },
@@ -14,37 +22,42 @@ AQI_BREAKPOINTS = {
     ],
 }
 PARTICLE_PROPS = ['pm1_0_atm', 'pm2_5_atm', 'pm10_0_atm']
+# fmt: on
 
+# fmt: off
 # Map of sensors to create entities for
 SENSORS_MAP = {
-    'pm2_5_aqi_a_raw':         {'key': 'pm2_5_aqi_raw',    'uom': DEVICE_CLASS_AQI, 'icon': 'mdi:blur-linear'},
-    'pm2_5_aqi_b_raw':         {'key': 'pm2_5_aqi_b_raw',  'uom': DEVICE_CLASS_AQI, 'icon': 'mdi:blur-linear'},
-    'pm2_5_atm_confidence':    {'key': 'pm2_5_atm_conf',   'uom': None,             'icon': 'mdi:seal'},
-    'particulate_matter_0_1':  {'key': 'pm1_0_atm',        'uom': DEVICE_CLASS_AQI, 'icon': 'mdi:blur'},
-    'particulate_matter_2_5':  {'key': 'pm2_5_atm',        'uom': DEVICE_CLASS_AQI, 'icon': 'mdi:blur'},
-    'particulate_matter_10':   {'key': 'pm10_0_atm',       'uom': DEVICE_CLASS_AQI, 'icon': 'mdi:blur'},
-    'air_quality_index_epa':   {'key': 'aqi_epa',          'uom': DEVICE_CLASS_AQI, 'icon': 'mdi:weather-hazy'},
-    'air_quality_index_lrapa': {'key': 'aqi_lrapa',        'uom': DEVICE_CLASS_AQI, 'icon': 'mdi:weather-hazy'},
-    'humidity':                {'key': 'current_humidity', 'uom': PERCENTAGE,       'icon': 'mdi:water-percent'},
-    'temperature':             {'key': 'current_temp',     'uom': TEMP_FAHRENHEIT,  'icon': 'mdi:thermometer'},
-    'dewpoint':                {'key': 'current_dewpoint', 'uom': TEMP_FAHRENHEIT,  'icon': 'mdi:water-outline'},
-    'pressure':                {'key': 'pressure',         'uom': PRESSURE_HPA,     'icon': 'mdi:gauge'},
+    'pm2_5_aqi_a_raw':         {'key': 'pm2_5_aqi_raw',    'uom': SensorDeviceClass.AQI,              'icon': 'mdi:blur-linear'},
+    'pm2_5_aqi_b_raw':         {'key': 'pm2_5_aqi_b_raw',  'uom': SensorDeviceClass.AQI,              'icon': 'mdi:blur-linear'},
+    'pm2_5_atm_confidence':    {'key': 'pm2_5_atm_conf',   'uom': None,                               'icon': 'mdi:seal'},
+    'particulate_matter_0_1':  {'key': 'pm1_0_atm',        'uom': SensorDeviceClass.AQI,              'icon': 'mdi:blur'},
+    'particulate_matter_2_5':  {'key': 'pm2_5_atm',        'uom': SensorDeviceClass.AQI,              'icon': 'mdi:blur'},
+    'particulate_matter_10':   {'key': 'pm10_0_atm',       'uom': SensorDeviceClass.AQI,              'icon': 'mdi:blur'},
+    'air_quality_index_epa':   {'key': 'aqi_epa',          'uom': SensorDeviceClass.AQI,              'icon': 'mdi:weather-hazy'},
+    'air_quality_index_lrapa': {'key': 'aqi_lrapa',        'uom': SensorDeviceClass.AQI,              'icon': 'mdi:weather-hazy'},
+    'humidity':                {'key': 'current_humidity', 'uom': PERCENTAGE,                         'icon': 'mdi:water-percent'},
+    'temperature':             {'key': 'current_temp',     'uom': UnitOfTemperature.FAHRENHEIT,       'icon': 'mdi:thermometer'},
+    'dewpoint':                {'key': 'current_dewpoint', 'uom': UnitOfTemperature.FAHRENHEIT,       'icon': 'mdi:water-outline'},
+    'pressure':                {'key': 'pressure',         'uom': UnitOfPressure.HPA,                 'icon': 'mdi:gauge'},
     'rssi':                    {'key': 'rssi',             'uom': SIGNAL_STRENGTH_DECIBELS_MILLIWATT, 'icon': 'mdi:wifi'}
 }
 SENSORS_DUAL_ONLY = ['pm2_5_aqi_b_raw']
+# fmt: on
 
-MANUFACTURER = 'Purple Air'
-DISPATCHER_PURPLE_AIR = 'dispatcher_purple_air'
+MANUFACTURER = "Purple Air"
+DISPATCHER_PURPLE_AIR = "dispatcher_purple_air"
 DOMAIN = "purpleair"
 TEMP_ADJUSTMENT = -8  # From PurpleAir javascript: `(parseInt(temp) + -8).toFixed(0);`
-HUMIDITY_ADJUSTMENT = +4  # From PurpleAir javascript: `(hum = parseInt(hum) + 4) > 100 && (hum = 100)`
+HUMIDITY_ADJUSTMENT = (
+    +4
+)  # From PurpleAir javascript: `(hum = parseInt(hum) + 4) > 100 && (hum = 100)`
 
 LOCAL_SCAN_INTERVAL = 30
 LOCAL_URL_FORMAT = "http://{0}/json?live=false"
 
 # Models
-PMS_SENSOR = 'PMS'
-BME_SENSOR = 'BME'
-MODEL_PA_1 = 'PA-I'
-MODEL_PA_2 = 'PA-II'
-MODEL_PA_FLEX = 'PA-II-FLEX'
+PMS_SENSOR = "PMS"
+BME_SENSOR = "BME"
+MODEL_PA_1 = "PA-I"
+MODEL_PA_2 = "PA-II"
+MODEL_PA_FLEX = "PA-II-FLEX"
